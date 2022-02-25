3b1b4f1f60b1561416d775ce26b27995b562ada3
Arijit <arijitmaiti085@gmail.com>
2022-02-25 18:33:07 +0530

    fustration!!!

--- logic.js
+++ logic.js
@@ -9,22 +9,22 @@ window.addEventListener('load', () => {
     }, 1500)
 });
 
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
     let scrollTop = document.getElementById('scrollTop');
     let scroll = document.documentElement.scrollTop;
     scrollTop.classList.toggle('active', scroll >= 400);
 });
 
 let NavIcon = document.getElementById('NavIcon');
-NavIcon.addEventListener('click', ()=>{
+NavIcon.addEventListener('click', () => {
     let downHeader = document.getElementById('downHeader');
     let wholeHeader = document.querySelector('#wholeHeader');
     wholeHeader.classList.toggle('active')
     downHeader.classList.toggle('active');
     NavIcon.classList.toggle('active');
-    if(NavIcon.classList.contains('active')){
-       NavIcon.innerHTML = `<i class="fas fa-times"></i>`
-    }else{
+    if (NavIcon.classList.contains('active')) {
+        NavIcon.innerHTML = `<i class="fas fa-times"></i>`
+    } else {
         NavIcon.innerHTML = `<i class="fas fa-bars"></i>`
     }
 });
@@ -36,7 +36,7 @@ NavIcon.addEventListener('click', ()=>{
 // })
 
 // scrolltop 
-function toTop(){
+function toTop() {
     document.documentElement.scrollTop = 0;
 }
 
@@ -44,14 +44,40 @@ function toTop(){
 let serachCliked_box = document.querySelector('.serachCliked_box'),
     searchBoxAll = document.querySelectorAll('.searchBoxAll'),
     search_cross = document.getElementById('search_cross');
-    // searchBoxAll.addEventListener('click', ()=>{
-    //     serachCliked_box.classList.add('active');
-    // });
-    search_cross.addEventListener('click', ()=>{
-        serachCliked_box.classList.remove('active');
-    });
-    searchBoxAll.forEach((e)=>{
-        e.addEventListener('click', ()=>{
-            serachCliked_box.classList.add('active');
-        })
+// searchBoxAll.addEventListener('click', ()=>{
+//     serachCliked_box.classList.add('active');
+// });
+search_cross.addEventListener('click', () => {
+    serachCliked_box.classList.remove('active');
+});
+searchBoxAll.forEach((e) => {
+    e.addEventListener('click', () => {
+        serachCliked_box.classList.add('active');
     })
+})
+
+// active nav on load 
+let currentLocation = location.href;
+let locationArr = currentLocation.split('/');
+let endpoint = locationArr[locationArr.length - 1];
+
+let nav_link = document.querySelectorAll('[data-endpoint]');
+
+let data_value = [...nav_link].map(e => e.getAttribute('data-endpoint'));
+
+nav_link.forEach((e, i) => {
+    if(data_value[i].includes('|')){
+        let newArr = data_value[i].split('|');
+        newArr.forEach((elem)=>{
+            if(elem == endpoint){
+                e.classList.add('active_nav_link');
+            }
+        })
+    }    
+    if (data_value[i] == endpoint) {
+        e.classList.add('active_nav_link');
+    }
+});
+
+
+
